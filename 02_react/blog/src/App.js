68caa236fa7bedde3e70756dd70b481ee051bfa1
sango0426 <93635656+sango0426@users.noreply.github.com>
2024-06-18 20:28:21 +0900

    24.06.18

--- 02_react/blog/src/App.js
+++ 02_react/blog/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let post = "강남 우동 맛집";
+  let [title, title2] = useState(['남자 코트 추천', '강남 우동맛집', '리액트 독학']);
+  let [like, like2] = useState(0);
+
+  function 함수() {
+    console.log(1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>React Blog</h4>
+      </div>
+      
+      <button onClick={()=>{
+        let copy = [...title];
+        copy.sort();
+        title2(copy);
+      }}>가나다순 정렬</button>
+
+
+      <div className="list">
+        <h4>{ title[0] } <span onClick={()=>{ like2(like + 1); } }>👍</span> { like } </h4>
+        <p>2월 17일 발행</p>
+        <button onClick={()=>{
+
+          let arr = [1, 2, 3];
+
+          
+          // state가 array/object 형식이면 독립적 카피본을 만들어서 수정
+          let copy = [...title];
+          copy[0] = '여자 코트 추천';
+          title2(copy);
+          }}>글 제목 바꾸기</button>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
