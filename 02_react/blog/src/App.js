7a70e0fe0bf64c33bc01b4b871a02fa17db42168
sango0426 <93635656+sango0426@users.noreply.github.com>
2024-06-19 19:07:16 +0900

    24.06.19

--- 02_react/blog/src/App.js
+++ 02_react/blog/src/App.js
@@ -7,6 +7,7 @@ function App() {
   let post = "강남 우동 맛집";
   let [title, title2] = useState(['남자 코트 추천', '강남 우동맛집', '리액트 독학']);
   let [like, like2] = useState(0);
+  let [modal, setModal] = useState(false);
 
   function 함수() {
     console.log(1);
@@ -17,38 +18,57 @@ function App() {
       <div className="black-nav">
         <h4>React Blog</h4>
       </div>
-      
       <button onClick={()=>{
         let copy = [...title];
         copy.sort();
         title2(copy);
       }}>가나다순 정렬</button>
 
-
       <div className="list">
         <h4>{ title[0] } <span onClick={()=>{ like2(like + 1); } }>👍</span> { like } </h4>
         <p>2월 17일 발행</p>
         <button onClick={()=>{
-
-          let arr = [1, 2, 3];
-
-          
           // state가 array/object 형식이면 독립적 카피본을 만들어서 수정
           let copy = [...title];
           copy[0] = '여자 코트 추천';
           title2(copy);
           }}>글 제목 바꾸기</button>
       </div>
+
       <div className="list">
         <h4>{ title[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
       <div className="list">
-        <h4>{ title[2] }</h4>
+        <h4 onClick={()=>{
+          modal == true ? setModal(false) : setModal(true)
+        }}>{ title[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      {
+        /* 조건식 ? 참일때 실행할 코드 : 거짓일 때 실행할 코드 */
+        modal == true ? <Modal/> : null
+      }
+
     </div>
   );
 }
+
+function Modal(){
+  return(
+    <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  );
+}
+
+/* [동적인 UI 만드는 3 step]
+1. html css로 미리 디자인 완성
+2. UI의 현재 상태를 state로 저장
+3. statedp 따라 UI가 어떻게 동작할지 코딩 */
 
 export default App;
